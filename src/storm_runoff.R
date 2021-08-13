56589d8d9fa5f0aa58beb587496d9c071f179cdc
danielkerstan <danielkerstan@bren.ucsb.edu>
2021-08-13 15:31:09 -0700

    finished task 2

---
+++ src/storm_runoff.R
@@ -0,0 +1,14 @@
+#' Predict runoff
+#'
+#' @param imper_frac number that indicates fraction of the watershed that is considered "impervious"
+#' @param watershed_area number that indicates the area of the watershed area
+#'
+#' @return
+#' @export
+#'
+#' @examples
+predict_runoff <- function(imper_frac, watershed_area) {
+  Rv = 0.05 + 0.9 * imper_frac
+  V = 3630 * 1 * Rv * watershed_area
+  return(V)
+}
